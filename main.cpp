7a269f5f7f4f5053b880c4d87775a0e5d24ef42d
Gabby <GabbyVByers@gmail.com>
2025-09-16 17:26:31 -0700

    quick test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 
 #include <iostream>
 
+
+
 int main()
 {
+
+	unsigned int accumulator = 0;
+	for (int i = 0; i < 30; i++)
+	{
+		accumulator += std::pow(2, i);
+		std::cout << (unsigned)accumulator << "\n";
+	}
+
 	std::cout << "Hello World!\n";
 	return 0;
 }
